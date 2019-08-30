9b8559f2ab98310fb1291dd86cffcec4c421c2db
danny <danny75@gmail.com>
2019-08-30 13:37:30 +0800

    增加MRTSation的處理

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import { stationList } from './station-list.const';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MRTStationList';
+
+    list = stationList;
+
 }
---
+++ src/app/station-list.const.ts
@@ -0,0 +1,196 @@
+export const stationList = [
+    {
+      Station: '七張站',
+      Destination: '松山站',
+      UpdateTime: '2019-08-29T20:07:53'
+    },
+    {
+      Station: '七張站',
+      Destination: '新店站',
+      UpdateTime: '2019-08-29T20:08:01.96'
+    },
+    {
+      Station: '大安站',
+      Destination: '淡水站',
+      UpdateTime: '2019-08-29T20:07:35.377'
+    },
+    {
+      Station: '大安站',
+      Destination: '象山站',
+      UpdateTime: '2019-08-29T20:07:55.88'
+    },
+    {
+      Station: '中山站',
+      Destination: '淡水站',
+      UpdateTime: '2019-08-29T20:07:35.377'
+    },
+    {
+      Station: '中山站',
+      Destination: '象山站',
+      UpdateTime: '2019-08-29T20:07:35.377'
+    },
+    {
+      Station: '中山國中站',
+      Destination: '動物園站',
+      UpdateTime: '2019-08-29T20:07:57'
+    },
+    {
+      Station: '內湖站',
+      Destination: '動物園站',
+      UpdateTime: '2019-08-29T20:07:57'
+    },
+    {
+      Station: '公館站',
+      Destination: '松山站',
+      UpdateTime: '2019-08-29T20:08:01.96'
+    },
+    {
+      Station: '北門站',
+      Destination: '松山站',
+      UpdateTime: '2019-08-29T20:08:01.96'
+    },
+    {
+      Station: '北門站',
+      Destination: '新店站',
+      UpdateTime: '2019-08-29T20:08:01.96'
+    },
+    {
+      Station: '台大醫院站',
+      Destination: '大安站',
+      UpdateTime: '2019-08-29T20:07:55.88'
+    },
+    {
+      Station: '台大醫院站',
+      Destination: '北投站',
+      UpdateTime: '2019-08-29T20:07:55.88'
+    },
+    {
+      Station: '民權西路站',
+      Destination: '南勢角站',
+      UpdateTime: '2019-08-29T20:07:39.427'
+    },
+    {
+      Station: '江子翠站',
+      Destination: '頂埔站',
+      UpdateTime: '2019-08-29T20:07:56.893'
+    },
+    {
+      Station: '西門站',
+      Destination: '南港展覽館站',
+      UpdateTime: '2019-08-29T20:07:56.893'
+    },
+    {
+      Station: '西門站',
+      Destination: '頂埔站',
+      UpdateTime: '2019-08-29T20:07:56.893'
+    },
+    {
+      Station: '西湖站',
+      Destination: '南港展覽館站',
+      UpdateTime: '2019-08-29T20:07:57'
+    },
+    {
+      Station: '亞東醫院站',
+      Destination: '南港展覽館站',
+      UpdateTime: '2019-08-29T20:07:46.527'
+    },
+    {
+      Station: '奇岩站',
+      Destination: '淡水站',
+      UpdateTime: '2019-08-29T20:07:55.88'
+    },
+    {
+      Station: '忠孝復興站',
+      Destination: '動物園站',
+      UpdateTime: '2019-08-29T20:07:57'
+    },
+    {
+      Station: '忠孝新生站',
+      Destination: '南勢角站',
+      UpdateTime: '2019-08-29T20:07:39.427'
+    },
+    {
+      Station: '東門站',
+      Destination: '北投站',
+      UpdateTime: '2019-08-29T20:07:35.377'
+    },
+    {
+      Station: '板橋站',
+      Destination: '亞東醫院站',
+      UpdateTime: '2019-08-29T20:07:56.893'
+    },
+    {
+      Station: '板橋站',
+      Destination: '南港展覽館站',
+      UpdateTime: '2019-08-29T20:07:56.893'
+    },
+    {
+      Station: '松山機場站',
+      Destination: '南港展覽館站',
+      UpdateTime: '2019-08-29T20:07:57'
+    },
+    {
+      Station: '芝山站',
+      Destination: '淡水站',
+      UpdateTime: '2019-08-29T20:07:55.88'
+    },
+    {
+      Station: '南京三民站',
+      Destination: '新店站',
+      UpdateTime: '2019-08-29T20:08:01.96'
+    },
+    {
+      Station: '南京復興站',
+      Destination: '台電大樓站',
+      UpdateTime: '2019-08-29T20:08:01.96'
+    },
+    {
+      Station: '南京復興站',
+      Destination: '松山站',
+      UpdateTime: '2019-08-29T20:07:53'
+    },
+    {
+      Station: '南港軟體園區站',
+      Destination: '南港展覽館站',
+      UpdateTime: '2019-08-29T20:07:57'
+    },
+    {
+      Station: '迴龍站',
+      Destination: '南勢角站',
+      UpdateTime: '2019-08-29T20:07:58.23'
+    },
+    {
+      Station: '頂溪站',
+      Destination: '南勢角站',
+      UpdateTime: '2019-08-29T20:07:39.427'
+    },
+    {
+      Station: '頂溪站',
+      Destination: '迴龍站',
+      UpdateTime: '2019-08-29T20:07:39.427'
+    },
+    {
+      Station: '新莊站',
+      Destination: '南勢角站',
+      UpdateTime: '2019-08-29T20:07:39.427'
+    },
+    {
+      Station: '萬芳社區站',
+      Destination: '動物園站',
+      UpdateTime: '2019-08-29T20:07:57'
+    },
+    {
+      Station: '葫洲站',
+      Destination: '動物園站',
+      UpdateTime: '2019-08-29T20:07:57'
+    },
+    {
+      Station: '輔大站',
+      Destination: '迴龍站',
+      UpdateTime: '2019-08-29T20:07:39.427'
+    },
+    {
+      Station: '劍潭站',
+      Destination: '象山站',
+      UpdateTime: '2019-08-29T20:07:35.377'
+    }];
